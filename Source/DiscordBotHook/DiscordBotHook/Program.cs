5dfb690a2c5b4f252fe7b8249d15df984144a2a9
Neeraj Anil <neerajan@microsoft.com>
2017-09-14 21:54:55 +0530

    Bot hook console working with post onto webapi
    Modified console to post message and token onto webapi.
    Modified webapi to receive post via json rather than get as message
    can be large.

--- Source/DiscordBotHook/DiscordBotHook/Program.cs
+++ Source/DiscordBotHook/DiscordBotHook/Program.cs
@@ -1,15 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace DiscordBotHook
+namespace DiscordWebHook
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            //for debugging
+            //args = new string[] {
+            //    "http://localhost:5123/api/macro",
+            //    "?chatwheel crybaby"
+            //};
+
+            if (args.Length < 2)
+                return 0;
+
+            try
+            {
+                string hookUrl = args[0];
+                string messageContent = args[1];
+
+                try
+                {
+                    Uri hookUri = new Uri(hookUrl);
+                }
+                catch (System.UriFormatException)
+                {
+                    MessageBox.Show("Error: Discord webhook url not in correct format.");
+                    return 0;
+                }
+
+
+                using (var client = new HttpClient())
+                {
+                    client.Timeout = new TimeSpan(0, 1, 0, 0);
+
+
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    var body = new StringContent(String.Format("{{ \"message\":\"{0}\",\"tokenid\":\"{1}\" }}", messageContent, "19cbda5b-b518-4d64-8e6c-edf3cd360306"));
+                    body.Headers.ContentType.MediaType = "application/json";
+
+                    var response = client.PostAsync(hookUrl, body).Result;
+                    return response.IsSuccessStatusCode ? 1 : 0;
+                }
+
+            }
+            catch
+            {
+                return 0;
+            }
         }
     }
 }
---
+++ Source/DiscordBotHook/MacroBot/Controllers/MacroController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MacroBot;
+
+namespace MacroBot.Controllers
+{
+
+    public class MacroRequest
+    {
+        public string tokenid {get;set;}
+        public string message {get;set;}
+    }
+
+    [Route("api/[controller]")]
+    public class MacroController : Controller
+    {
+        [HttpPost]
+        public async Task Post([FromBody]MacroRequest request)
+        {
+            var ts = new TokenDataService(new LiteDbDatabaseFactory());
+            var token  = await ts.GetToken(request.tokenid);
+            await GlobalState.CurrentBot.SendMessageAsync(token.ChannelId, request.message);
+        }
+    }
+}
