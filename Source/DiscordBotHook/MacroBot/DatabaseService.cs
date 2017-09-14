0bbb0cee9d71a16f31ff16a9894c0022a4717560
Neeraj Anil <neerajanil@gmail.com>
2017-09-14 10:52:49 +0530

    Added code for basic operations in LiteDb

--- Source/DiscordBotHook/MacroBot/DatabaseService.cs
+++ Source/DiscordBotHook/MacroBot/DatabaseService.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LiteDB;
 
 namespace MacroBot
 {
     public class Token
     {
+        [BsonId]
         public string Id { get; set; }
         public ulong UserId { get; set; }
         public ulong ChannelId { get; set; }
@@ -16,35 +18,71 @@ namespace MacroBot
 
     public interface IDatabaseService
     {
-        Task<Token> GetToken(string Token);
-        Task DeleteToken(string Token);
-        Task DeleteToken(ulong UserId);
+        Task<Token> GetToken(string tokenId);
+        Task DeleteToken(string tokenId);
+        Task DeleteToken(ulong userId);
         Task InsertToken(Token token);
     }
 
 
     public class LiteDbDatabaseService : IDatabaseService
     {
-        public LiteDbDatabaseService(){}
-        
+        private const string _tokenCollectionName = "tokens";
+        private const string _tokenDatabaseName = @"tokens.db";
 
-        public Task<Token> GetToken(string Token)
+        public LiteDbDatabaseService()
         {
-            return Task.FromResult<Token>(new Token());
+            using(var db = new LiteDatabase(_tokenDatabaseName))
+            {
+                // Get customer collection
+                var tokens = db.GetCollection<Token>(_tokenCollectionName);
+                tokens.EnsureIndex(t => t.Id);
+                tokens.EnsureIndex(t => t.UserId);
+            }
         }
         
-        public Task DeleteToken(string Token)
+        public Task<Token> GetToken(string tokenId)
         {
+            Token token = null;
+            using(var db = new LiteDatabase(_tokenDatabaseName))
+            {
+                // Get customer collection
+                var tokens = db.GetCollection<Token>(_tokenCollectionName);
+                token = tokens.Find(x => x.Id == tokenId).FirstOrDefault();
+            }
+            return Task.FromResult<Token>(token);
+        }
+        
+        public Task DeleteToken(string tokenId)
+        {
+            using(var db = new LiteDatabase(_tokenDatabaseName))
+            {
+                // Get customer collection
+                var tokens = db.GetCollection<Token>(_tokenCollectionName);
+                tokens.Delete(tokenId);
+            }
             return Task.CompletedTask;
         }
         
-        public Task DeleteToken(ulong UserId)
+        public Task DeleteToken(ulong userId)
         {
+            using(var db = new LiteDatabase(_tokenDatabaseName))
+            {
+                // Get customer collection
+                var tokens = db.GetCollection<Token>(_tokenCollectionName);
+                tokens.Delete(x => x.UserId == userId);
+            }
             return Task.CompletedTask;
         }
 
         public Task InsertToken(Token token)
         {
+            using(var db = new LiteDatabase(_tokenDatabaseName))
+            {
+                // Get customer collection
+                var tokens = db.GetCollection<Token>(_tokenCollectionName);
+                tokens.Insert(token);
+            }
             return Task.CompletedTask;
         }
 
